f9995e067e432c4a6c506eedb367052598f785ce
Koushik007321 <koushikdutta620@gmail.com>
2025-07-26 14:02:29 +0530

    Any

--- src/app/Login-Signup/page.jsx
+++ src/app/Login-Signup/page.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import LoginSignupPage from "@/components/NewLoginPage";
 
-
-import localFont from "next/font/local"; // Import local font
-
-const myFont = localFont({ src: "../../fonts/JungleFlame-nAAv4.otf" });
-
 export default function page() {
   return (
     <div >
